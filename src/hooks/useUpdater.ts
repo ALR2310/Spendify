8936a9065381390fc11534aa0e9151e30a03c556
An Thanh Le <an.thanhle@infodation.vn>
2025-12-30 09:25:57 +0700

    clean old code

--- src/hooks/useUpdater.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-
-import { UpdaterContext } from '~/providers/UpdaterProvider';
-
-export const useUpdater = () => {
-  const context = useContext(UpdaterContext);
-  if (!context) throw new Error('useUpdater must be used within a UpdaterProvider');
-  return context;
-};
